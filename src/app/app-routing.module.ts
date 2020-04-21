143399e5d2c7e80ed75ad5f6a782c6cce16248f0
ABD. MALIK <malikzeros@gmail.com>
2020-04-21 17:38:40 +0700

    belajar angular dengan npm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// start this
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+// end this
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // start this
+  { path: "", pathMatch: "full", redirectTo: "home" },
+  { path: "home", component: HomeComponent },
+  { path: "contact-create", component: ContactCreateComponent },
+  { path: "contact-list", component: ContactListComponent },
+  { path: 'products', component: ProductComponent, data: { title: 'Product List' } },
+  { path: 'product-details/:id', component: ProductDetailComponent, data: { title: 'Product Details' } },
+  { path: 'product-add', component: ProductAddComponent, data: { title: 'Product Add' } },
+  { path: 'product-edit/:id', component: ProductEditComponent, data: { title: 'Product Edit' } }
+  // end this
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,69 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+// add this
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductComponent } from './product/product.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent,
+    data: { title: 'Product List' }
+  },
+  {
+    path: 'product-details/:id',
+    component: ProductDetailComponent,
+    data: { title: 'Product Details' }
+  },
+  {
+    path: 'product-add',
+    component: ProductAddComponent,
+    data: { title: 'Product Add' }
+  },
+  {
+    path: 'product-edit/:id',
+    component: ProductEditComponent,
+    data: { title: 'Product Edit' }
+  },
+  { path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactCreateComponent,
+    ContactListComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductComponent,
+    ProductAddComponent,
+    ProductDetailComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // add this
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
